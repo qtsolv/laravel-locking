3d702630999f69632c9f23aef3b8b080d2a883fe
Vaibhav Pandey <contact@vaibhavpandey.com>
2022-06-21 16:39:32 +0530

    Fix blade to php generation.

--- src/LockingServiceProvider.php
+++ src/LockingServiceProvider.php
@@ -35,18 +35,12 @@ class LockingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('lockInput', function (Model $model) {
-            $valid = in_array(LocksVersion::class, class_uses_recursive($model), true);
-            if (!$valid) {
-                throw new \RuntimeException(
-                    sprintf('%s does not use %s trait.', get_class($model), LocksVersion::class)
-                );
-            }
-
-            /** @var LocksVersion $model */
-            $fieldName = call_user_func(get_class($model), 'lockVersionColumnName');
-            $fieldValue = $model->currentLockVersion();
-            return sprintf('<input type="hidden" name="%s" value="%s">', $fieldName, $fieldValue);
+        Blade::directive('lockInput', function ($model) {
+            return sprintf(
+                '<input type="hidden" name="<?= %s ?>" value="<?= %s ?>">',
+                "call_user_func([get_class($model), 'lockVersionColumnName'])",
+                "{$model}->currentLockVersion()"
+            );
         });
     }
 }
